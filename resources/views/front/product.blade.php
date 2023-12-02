fe6d81d043ee673539ac5972a2950c1eb232744c
MynkMobilise <mayank.sabharwal@mobilise.co.in>
2023-12-02 22:03:06 +0530

    all

--- resources/views/front/product.blade.php
+++ resources/views/front/product.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid px-lg-5 px-md-5 px-4">
             <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
             <ol class="breadcrumb mb-0">
-                <li class="breadcrumb-item small"><a class="mclr" href="/"></a></li>
+                <li class="breadcrumb-item small"><a class="mclr" href="/">Home</a></li>
                 <li class="breadcrumb-item active small" aria-current="page">{{$product[0]->name}}</li>
             </ol>
             </nav>
